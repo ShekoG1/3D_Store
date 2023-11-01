98f5edb6e802633e643381502784961bcd67fed9
Shekhar Maharaj <shekharmaharaj2905@gmail.com>
2023-11-01 19:57:51 +0200

    Added and completed server logic for image generation using openAI DallE

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import RegularButton from './RegularButton'
 
-const AIPicker = () => {
+const AIPicker = ({prompt,setPrompt,generatingImg,handleSubmit}) => {
   return (
-    <div>AIPicker</div>
+    <div className='aipicker-container'>
+      <textarea placeholder='Ask AI' className='aipicker-textarea' cols="30" rows="5" value={prompt} onChange={(e)=> setPrompt(e.target.value)}>
+
+      </textarea>
+
+      <div className='flex flex-wrap gap-3'>
+        {generatingImg ? (<RegularButton/>): (<></>)}
+      </div>
+    </div>
   )
 }
 
